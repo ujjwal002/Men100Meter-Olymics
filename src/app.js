81fa4a00ececd889f9283313bbb66e2710221614
Ujjwal Kumar <ujjwalsinghrajput2000@gmail.com>
2023-03-23 16:02:18 +0530

    connecting nodejs with mongoose

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+require("../src/db/conn.js");
 const port = process.env.PORT || 3000;
 
 app.get("/", async (req, res) => {
--- src/db/conn.js
+++ src/db/conn.js
@@ -1,3 +1,12 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb//localhost:27017/olymics");
+const connect = async () => {
+  try {
+    const res = await mongoose.connect("mongodb://localhost:27017/olymics");
+    console.log("connecton of nodejs with mongoose succesful");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connect();
