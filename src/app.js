27ca6f313beba76f594e0a96f3cb121d9543759a
Ujjwal Kumar <ujjwalsinghrajput2000@gmail.com>
2023-03-23 17:05:35 +0530

    added all the http METHOD

--- src/app.js
+++ src/app.js
@@ -1,10 +1,64 @@
 const express = require("express");
+const MensRanking = require("../src/models/mens");
 const app = express();
 require("../src/db/conn.js");
+const MenRanking = require("../src/models/mens");
 const port = process.env.PORT || 3000;
 
-app.get("/", async (req, res) => {
-  res.send("hello from the ujjwal technical");
+app.use(express.json());
+
+app.post("/mens", async (req, res) => {
+  try {
+    const addingMensRecors = new MensRanking(req.body);
+    console.log(req.body);
+    const insertMens = await addingMensRecors.save();
+    res.status(200).send(insertMens);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.get("/mens", async (req, res) => {
+  try {
+    const getMens = await MenRanking.find({}).sort({ ranking: 1 });
+    res.status(200).send(getMens);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+// get request of individul
+app.get("/mens/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const getMen = await MenRanking.findById(_id);
+    res.status(200).send(getMen);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// we will handes patch req of individual(patch)
+
+app.patch("/mens/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const getMen = await MenRanking.findByIdAndUpdate(_id, req.body, {
+      new: true,
+    });
+    res.status(200).send(getMen);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.delete("/mens/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const getMen = await MenRanking.findByIdAndDelete(_id);
+    res.status(200).send(getMen);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 const connection = async () => {
