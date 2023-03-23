5f229b50ae8d59cd6687d5a5d07bfd67ec6d6570
Ujjwal Kumar <ujjwalsinghrajput2000@gmail.com>
2023-03-23 15:54:06 +0530

    creating all the src,model and router then connectiong to mongodb

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", async (req, res) => {
+  res.send("hello from the ujjwal technical");
+});
+
+const connection = async () => {
+  await app.listen(port, () => {
+    console.log(`connection live at port no.${port}`);
+  });
+};
+connection();
